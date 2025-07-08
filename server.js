cd13ee4a91cd23a12d4ec6b947203d43738cb7ff
Benny7412 <bennyklaus@gmail.com>
2025-07-07 20:06:31 -0500

    added roputes

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
-const lesson1Controller = require("./controllers/lesson1");
-
-app.get("/", lesson1Controller.emilyRoute);
-
-app.get("/blah", lesson1Controller.blahRoute);
 
 const port = 3000;
 
-app.listen(process.env.port || port, () => {
-  console.log("Server is running at port " + (process.env.port || port));
-});
+app.use("/", require("./routes"));
+
+app.listen(process.env.port || port);
+console.log("Server is running at port " + (process.env.port || port));
